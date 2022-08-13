672567f182068b734010e82d28f4e2e9ad5c0f2c
이지영 <jssq2468@naver.com>
2022-08-13 20:04:26 +0900

    feat : getTodos 구현

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import Main from "./pages/Main";
 import SingUp from "./pages/Signup";
 import SingIn from "./pages/Signin";
-import Todolist from "./pages/Todolist";
+import Todo from "./pages/Todo";
 function App() {
   return (
     <div>
@@ -10,7 +10,7 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/signin" element={<SingIn />} />
         <Route path="/signup" element={<SingUp />} />
-        <Route path="/todo" element={<Todolist />} />
+        <Route path="/todo" element={<Todo />} />
       </Routes>
     </div>
   );
---
+++ src/api.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function api() {
+  return <div></div>;
+}
+
+export default api;
