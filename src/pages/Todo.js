9cc4edb6a8ede66046c904aacac66e691f65ea3c
이지영 <jssq2468@naver.com>
2022-08-15 20:20:15 +0900

    feat : useRef 로 특정 DOM 선택

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,4 +1,5 @@
-import React, { useCallback, useContext, useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "@emotion/styled";
 import axios from "axios";
 import TodoList from "./TodoLists";
@@ -7,9 +8,22 @@ function Todo() {
   const [listVal, setListVal] = useState();
   const [word, setWord] = useState(true);
 
+  const navigate = useNavigate();
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  if (localStorage.getItem("accessToken") == null) {
+    alert("로그인 해주세요!");
+    navigate("/");
+  }
   const inputTest = (e) => {
     setListVal(e.target.value);
   };
+
+  //get 요청
   useEffect(() => {
     axios
       .get(
@@ -30,7 +44,9 @@ function Todo() {
 
   //글작성
   const onSubmit = (e) => {
+    inputRef.current.focus();
     e.preventDefault();
+    setListVal("");
     const form = {
       todo: listVal,
     };
@@ -56,25 +72,66 @@ function Todo() {
   };
 
   return (
-    <div>
-      <>
-        <h2>Todolist</h2>
-        <list>
+    <Todolist>
+      <div className="list">
+        <div className="top">
+          <h2>Todolist</h2>
           <form onChange={inputTest}>
-            <input></input>
+            <input ref={inputRef} value={listVal}></input>
             <button onClick={onSubmit}>추가</button>
           </form>
-          <table>
-            <tbody>
-              {value.map((list) => (
-                <TodoList list={list} />
-              ))}
-            </tbody>
-          </table>
-        </list>
-      </>
-    </div>
+        </div>
+
+        <table>
+          <tbody>
+            {value.map((list) => (
+              <TodoList list={list} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </Todolist>
   );
 }
+const Todolist = styled.div`
+  .list {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    max-width: 800px;
+    margin: 0 auto;
+    min-height: 90vh;
+  }
+  .top {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 20px;
+    input {
+      width: 400px;
+      height: 32px;
+      font-size: 15px;
+      border: 0;
+      border-radius: 15px;
+      outline: none;
+      padding-left: 10px;
+      background-color: rgb(233, 233, 233);
+    }
+    button {
+      margin-left: 15px;
+      border: 1px solid grey;
+      box-shadow: 0 15px 35px rgba(0, 0, 0, 0.1);
+      font-weight: 600;
+      color: pink;
+      padding: 10px;
+    }
+  }
+  table {
+    margin: 0 auto;
+    border: 0.5px solid pink;
+    border-collapse: collapse;
+  }
+`;
 
 export default Todo;
