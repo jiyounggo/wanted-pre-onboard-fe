ccf220ce028cac84ab79302b5efa7a744f12a541
이지영 <jssq2468@naver.com>
2022-08-17 14:34:53 +0900

    style : 메인페이지 UI

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,20 +1,82 @@
 import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import styled from "@emotion/styled";
+
 function Main() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (window.localStorage.getItem("accessToken")) {
+    if (localStorage.getItem("accessToken")) {
       navigate("/todo");
-    } else if (!window.localStorage.getItem("accessToken")) {
+    } else if (!localStorage.getItem("accessToken")) {
       navigate("/");
     }
   }, []);
   return (
-    <div>
-      <div>메인임</div>
-    </div>
+    <Wrapper>
+      <div className="content">
+        <h2 className="welcome">WELCOME</h2>
+        <div className="welcomTxt">
+          <span>Welcome to the todo-list</span>
+        </div>
+        <div>
+          <Link to="/signin">
+            <button className="loginBtn">Login</button>
+          </Link>
+        </div>
+        <div>
+          <Link to="/signup">
+            <button className="regBtn">Register</button>
+          </Link>
+        </div>
+      </div>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background: #c1efff;
+  .content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    font-family: system-ui, serif;
+    font-size: 2rem;
+    padding: 2.5rem;
+    border-radius: 10px;
+    background: white;
+  }
+  .welcome {
+    color: #ffb3b3;
+    margin: 0;
+  }
+
+  .welcomTxt {
+    font-size: 20px;
+    margin: 20px 0;
+  }
+  .loginBtn,
+  .regBtn {
+    border: none;
+    border-radius: 20px;
+    width: 200px;
+    height: 30px;
+    color: white;
+    font-size: 15px;
+    font-weight: bold;
+    cursor: pointer;
+  }
+  .loginBtn {
+    background-color: #ffdba4;
+  }
+  .regBtn {
+    background-color: #ffe9ae;
+  }
+`;
 
 export default Main;
