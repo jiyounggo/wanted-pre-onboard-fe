d64e759b938d74a4b95a880f3d1512977b40da4b
이지영 <jssq2468@naver.com>
2022-08-17 14:35:22 +0900

    style : 로그인 페이지 UI

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,33 +1,36 @@
 import React from "react";
-import axios from "axios";
 import { useState } from "react";
-import { signIn } from "../api";
+import { signIn } from "../apis/auth";
 import { useNavigate, Link } from "react-router-dom";
+import styled from "@emotion/styled";
 
 function Signin() {
-  const [id, setId] = useState("");
-  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const inputId = (e) => {
-    setId(e.target.value);
-    console.log(e.target.value);
-  };
+  const [input, setInput] = useState({
+    signinId: "",
+    signinPw: "",
+  });
 
-  const inputPassword = (e) => {
-    setPassword(e.target.value);
+  const { signinId, signinPw } = input;
+
+  const onChangeInput = (e) => {
+    const { name, value } = e.target;
+    setInput({
+      ...input,
+      [name]: value,
+    });
   };
 
   //로그인 요청
   const submitsignin = async (e) => {
     const data = {
-      email: id,
-      password: password,
+      email: input.signinId,
+      password: input.signinPw,
     };
     e.preventDefault();
     signIn(data)
       .then((res) => {
-        console.log(res);
         localStorage.setItem("accessToken", res.data.access_token);
         navigate("/todo");
       })
@@ -43,12 +46,68 @@ function Signin() {
   };
 
   return (
-    <div>
-      <input type="text" onChange={inputId} placeholder="e-mail" />
-      <input type="password" onChange={inputPassword} placeholder="password" />
-      <button onClick={submitsignin}>로그인</button>
-    </div>
+    <SignIn>
+      <div className="content">
+        <div className="top">
+          <h2>Sign In</h2>
+          <input
+            name="signinId"
+            type="text"
+            onChange={onChangeInput}
+            placeholder="e-mail"
+            value={signinId}
+          />
+          <input
+            name="signinPw"
+            type="password"
+            onChange={onChangeInput}
+            placeholder="password"
+            value={signinPw}
+          />
+        </div>
+        <div className="signBtn">
+          <button onClick={submitsignin}>로그인</button>
+        </div>
+        <Link to="/signup">
+          <p className="bottom">회원가입 하러 가기</p>
+        </Link>
+      </div>
+    </SignIn>
   );
 }
+
+const SignIn = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 90vh;
+  flex-direction: column;
+  min-height: 100vh;
+  background: #c1efff;
+
+  .content {
+    width: 250px;
+    height: 300px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    border-radius: 10px;
+    background: white;
+    padding: 40px;
+  }
+  .signBtn button {
+    background-color: pink;
+    cursor: pointer;
+    border: none;
+    padding: 5px;
+    margin-top: 25px;
+    width: 60px;
+    border-radius: 10px;
+  }
+  .bottom {
+    font-size: 13px;
+  }
+`;
 
 export default Signin;
